f79c34606bc671be06ee3c3e35228ca85bfaf6aa
ogyogy <47146269+ogyogy@users.noreply.github.com>
2019-03-27 21:07:33 +0900

    コンストラクタで初期化処理を行うように変更

--- src/main/java/example/Activator.java
+++ src/main/java/example/Activator.java
@@ -2,21 +2,19 @@ package example;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.BundleReference;
-import org.osgi.framework.FrameworkUtil;
 
 public class Activator implements BundleActivator {
     private BundleContext bundleContext;
 
-    public void init() {
-        bundleContext = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
+    public Activator(BundleContext ctx) {
+        bundleContext = ctx;
     }
 
-    public void start(BundleContext context) {
+    public void start(BundleContext ctx) {
         System.out.println("Starting the bundle");
     }
 
-    public void stop(BundleContext context) {
+    public void stop(BundleContext cxt) {
         System.out.println("Stopping the bundle");
     }
 
