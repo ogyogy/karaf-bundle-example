1e6fc295ce58b47b74bde391120b3b2c9a315750
ogyogy <47146269+ogyogy@users.noreply.github.com>
2019-03-27 21:09:01 +0900

    importを整理

--- src/test/java/example/ActivatorTest.java
+++ src/test/java/example/ActivatorTest.java
@@ -1,36 +1,19 @@
 package example;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
-import org.junit.Rule;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Spy;
 import org.junit.Before;
 import org.junit.After;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
-
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-
-import org.apache.sling.testing.mock.osgi.MockOsgi;
-import org.mockito.internal.util.reflection.Whitebox;
-
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.*;
 import static org.junit.Assert.assertEquals;
-
+import org.osgi.framework.BundleContext;
+import org.apache.sling.testing.mock.osgi.MockOsgi;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class ActivatorTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    // private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
+    // private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
     // private final PrintStream originalErr = System.err;
-
     Activator activator;
     BundleContext bundleContext = MockOsgi.newBundleContext();
 
@@ -38,8 +21,8 @@ public class ActivatorTest {
     public void setup() throws Exception {
         System.setOut(new PrintStream(outContent));
         // System.setErr(new PrintStream(errContent));
-        activator = new Activator();
-        Whitebox.setInternalState(activator, "bundleContext", bundleContext);
+        activator = new Activator(bundleContext);
+        // Whitebox.setInternalState(activator, "bundleContext", bundleContext);
     }
 
     @After
